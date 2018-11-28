d44d4a220a8f4bcb46b00f8685c2ff1889526d18
Koen Lageveen <koen.lageveen@me.com>
2018-11-28 11:43:41 +0100

    play with some animels

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 var h = document.querySelector("header");
 var h_height = h.clientHeight;
+var animels = document.querySelectorAll(".animel, blockquote");
+
+function checkAnimelsInView(){
+	animels.forEach(function(i){
+		if (i.getBoundingClientRect().top < document.documentElement.clientHeight) {
+			i.classList.add("top-in-view");
+		}
+		if (i.getBoundingClientRect().bottom < document.documentElement.clientHeight) {
+			i.classList.add("bottom-in-view");
+		}
+	});
+}
+checkAnimelsInView();
 
 window.onscroll = function(){
 	if (window.scrollY > h_height) {
-		document.body.classList.add("whoop");
+		document.body.classList.add("header-out");
 	} else {
-		document.body.classList.remove("whoop");
+		document.body.classList.remove("header-out");
 	}
+	checkAnimelsInView();
 };
